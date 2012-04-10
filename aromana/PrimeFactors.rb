79c596b77c24aa7936109585baed64e80029da39
Arturo R <arturovrc@gmail.com>
2012-04-10 00:59:03 -0500

    primera validacion

--- aromana/PrimeFactors.rb
+++ aromana/PrimeFactors.rb
@@ -1,3 +1,20 @@
 class PrimeFactors
-	
+	attr_reader :PRIMO
+
+	def calculaPrimo(numero)
+		lista = []
+		index = 0
+
+		@PRIMO = 2
+
+
+		if numero % @PRIMO ==0 
+			numero = numero / @PRIMO
+			lista[index] = @PRIMO
+			index = index + 1
+		end
+
+		lista.sort
+
+	end
 end
--- aromana/PrimeFactors_spec.rb
+++ aromana/PrimeFactors_spec.rb
@@ -3,6 +3,7 @@ require "./PrimeFactors.rb"
 
 describe PrimeFactors do 
 	it "calcula primo" do
-	
+		primos = PrimeFactors.new
+		primos.calculaPrimo(6).should == [2] 
 	end
 end
